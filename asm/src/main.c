dcd11f495f1119c50ddee48a2426bbfd3d4ea4a1
HECTOR Guillaume <VendeurDePoulet@localhost.localdomain>
2018-02-05 21:30:16 +0100

    main asm modified to check syntax

--- asm/src/main.c
+++ asm/src/main.c
@@ -13,6 +13,7 @@ int main(int ac, char **av)
 	int bin = open(av[2], O_WRONLY | O_CREAT, 0444);
 	char *s;
 	char **tab;
+	int mnemonic;
 
 	(void)ac;
 	if (src == -1 || bin == -1)
@@ -20,7 +21,8 @@ int main(int ac, char **av)
 	while ((s = get_next_line(src))) {
 		tab = str_to_av(s);
 		print_tabtab(tab);
-		write_instruction(tab[0], bin);
+		mnemonic = write_instruction(tab[0], bin);
+		verif_syntax(mnemonic, tab);
 	}
 	return (0);
 }
