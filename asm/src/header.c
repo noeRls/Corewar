74225c095865983322c62499728e4dd300f5c4f8
Gabriel Colson <gabriel_c@localhost.localdomain>
2018-02-06 03:16:43 +0100

    set prog_size to 0

--- asm/src/header.c
+++ asm/src/header.c
@@ -10,6 +10,7 @@
 void fill_header(char **tab, header_t *header)
 {
 	header->magic = COREWAR_EXEC_MAGIC;
+	header->prog_size = 0;
 	if (!my_strcmp(tab[0], ".name"))
 	        my_strcpy(header->prog_name, tab[1]);
 	if (!my_strcmp(tab[0], ".comment"))
