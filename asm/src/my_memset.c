5a92f627146f10ac61f6394b075b7711db4d8b71
Gabriel Colson <gabriel_c@localhost.localdomain>
2018-02-06 03:37:42 +0100

    my_super_memset

---
+++ asm/src/my_memset.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** CPE_corewar_2017
+** File description:
+** my_memset
+*/
+
+#include "asm.h"
+
+void *my_super_memset(void *ptr, int c, size_t n)
+{
+	for (size_t i = 0; i < n; i++) {
+		*((char *)ptr) = c;
+	}
+	return (ptr);
+}
