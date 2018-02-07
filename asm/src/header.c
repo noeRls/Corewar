198ed83f82e6d658a2b3bc8c55cc322edae025fa
Gabriel Colson <gabriel_c@localhost.localdomain>
2018-02-07 21:37:16 +0100

    using comment and name defines

--- asm/src/header.c
+++ asm/src/header.c
@@ -10,11 +10,11 @@
 int fill_header(char **tab, header_t *header)
 {
 	header->magic = COREWAR_EXEC_MAGIC;
-	if (!my_strcmp(tab[0], ".name")) {
+	if (!my_strcmp(tab[0], NAME_CMD_STRING)) {
 		my_strcpy(header->prog_name, tab[1]);
 		return (1);
 	}
-	if (!my_strcmp(tab[0], ".comment")) {
+	if (!my_strcmp(tab[0], COMMENT_CMD_STRING)) {
 		my_strcpy(header->comment, tab[1]);
 		return (1);
 	}
