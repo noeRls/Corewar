679b8a39f01c7ca55a431f8fb415c271fccb65fc
HECTOR Guillaume <VendeurDePoulet@localhost.localdomain>
2018-02-08 18:29:08 +0100

    filling functions completed

--- asm/src/label.c
+++ asm/src/label.c
@@ -13,9 +13,11 @@ void fill_label_call(char *str, int fd, int mnemonique, label_t *label)
 
 	while (call->next)
 		call = call->next;
+	call->next = malloc(sizeof(call_t));
+	call = call->next;
 	call->name = my_strdup(str);
-	call->abs_pos = ;
-	call->rel_pos = ;
+	call->abs_pos = label->current_pos;
+	call->rel_pos = -1;
 	call->next = NULL;
 }
 
@@ -25,4 +27,10 @@ void fill_label_decla(char *str, int fd, int mnemonique, label_t *label)
 
 	while (decla->next)
 		decla = decla->next;
+	decla->next = malloc(sizeof(decla_t));
+	decla = decla->next;
+	decla->name = my_strdup(str);
+	decla->abs_pos = label->current_pos;
+	decla->rel_pos = -1;
+	decla->next = NULL;
 }
