8dba9296e2dd47532f59efee63c8b0cd596b948a
HECTOR Guillaume <VendeurDePoulet@localhost.localdomain>
2018-02-15 18:29:59 +0100

    type of args handled

--- asm/src/verif_syntax.c
+++ asm/src/verif_syntax.c
@@ -21,7 +21,22 @@ void verif_nb_arg(int mnemonic, char **tab)
 void verif_arg_type(int mnemonic, char **tab)
 {
 	for (int i = 1 ; tab[i] ; i++) {
-
+		if (tab[i][0] == DIRECT_CHAR) {
+			if ((op_tab[mnemonic].type[i - 1] & T_DIR) == 0) {
+				my_puterror("Invalid type of arguments\n");
+				exit(84);
+			}
+		} else if (tab[i][0] == 'r') {
+			if ((op_tab[mnemonic].type[i - 1] & T_REG) == 0) {
+				my_puterror("Invalid type of arguments\n");
+				exit(84);
+			}
+		} else {
+			if ((op_tab[mnemonic].type[i - 1] & T_IND) == 0) {
+				my_puterror("Invalid type of arguments\n");
+				exit(84);
+			}
+		}
 	}
 }
 
---
+++ asm/tests/syntax.s
@@ -0,0 +1,22 @@
+	# test de l encodage de toutes les fonctions basiques
+	#
+	#bismillah
+	.name "romain bosa"
+	.comment "just a basic prog to kick u from tek"
+
+	live %1
+	ld %4,r1
+	st r1,23
+	add r1,r2,r3
+	sub r2,r7,r2
+	and %23,40,r1
+	or 12,%1,r12
+	xor %32,%12,r1
+	zjmp %4
+	ldi %4,%12,%4
+	sti r1,r2,%4
+	fork %16
+	lld %4,r1
+	lldi %4,%12,r1
+	lfork %16
+	aff r3
