e73054dbc7fa7a53f9e6ed5a700760c5cb9a13e3
Gabriel Colson <gabriel_c@localhost.localdomain>
2018-02-17 17:55:03 +0100

    colors on ALL error messages

--- asm/src/error_handling/verif_syntax.c
+++ asm/src/error_handling/verif_syntax.c
@@ -13,7 +13,7 @@ void verif_nb_arg(int mnemonic, char **tab)
 
 	for (i = 0 ; tab[i] ; i++);
 	if (i - 1 != op_tab[mnemonic].nbr_args) {
-		my_puterror("invalid number of arguments\n");
+		my_puterror(ERROR"invalid number of arguments\n");
 		exit(84);
 	}
 }
@@ -23,17 +23,17 @@ void verif_arg_type(int mnemonic, char **tab)
 	for (int i = 1 ; tab[i] ; i++) {
 		if (tab[i][0] == DIRECT_CHAR) {
 			if ((op_tab[mnemonic].type[i - 1] & T_DIR) == 0) {
-				my_puterror("Invalid type of arguments\n");
+				my_puterror(ERROR"Invalid type of arguments\n");
 				exit(84);
 			}
 		} else if (tab[i][0] == 'r') {
 			if ((op_tab[mnemonic].type[i - 1] & T_REG) == 0) {
-				my_puterror("Invalid type of arguments\n");
+				my_puterror(ERROR"Invalid type of arguments\n");
 				exit(84);
 			}
 		} else {
 			if ((op_tab[mnemonic].type[i - 1] & T_IND) == 0) {
-				my_puterror("Invalid type of arguments\n");
+				my_puterror(ERROR"Invalid type of arguments\n");
 				exit(84);
 			}
 		}
